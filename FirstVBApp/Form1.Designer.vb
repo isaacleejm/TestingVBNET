d8e4d69a3334c5acc867e21df0c09a09751fed6e
isaacleejm <108120700+isaacleejm@users.noreply.github.com>
2025-09-03 01:20:28 +1000

    first commit

---
+++ FirstVBApp/Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnGo = New Button()
+        btnVariables = New Button()
+        btrDataTypes = New Button()
+        btnSubmit = New Button()
+        firstNameTextBox = New TextBox()
+        firstNameLabel = New Label()
+        lastNameLabel = New Label()
+        lastNameTextBox = New TextBox()
+        ageTextBox = New TextBox()
+        ageLabel = New Label()
+        occupationListBox = New ListBox()
+        numberTextBox2 = New TextBox()
+        numberTextBox1 = New TextBox()
+        twoNumberButton = New Button()
+        SuspendLayout()
+        ' 
+        ' btnGo
+        ' 
+        btnGo.Location = New Point(152, 103)
+        btnGo.Margin = New Padding(4, 5, 4, 5)
+        btnGo.Name = "btnGo"
+        btnGo.Size = New Size(254, 152)
+        btnGo.TabIndex = 0
+        btnGo.Text = "TextInput Button"
+        btnGo.UseVisualStyleBackColor = True
+        ' 
+        ' btnVariables
+        ' 
+        btnVariables.Location = New Point(447, 103)
+        btnVariables.Name = "btnVariables"
+        btnVariables.Size = New Size(253, 152)
+        btnVariables.TabIndex = 1
+        btnVariables.Text = "Button1"
+        btnVariables.UseVisualStyleBackColor = True
+        ' 
+        ' btrDataTypes
+        ' 
+        btrDataTypes.Location = New Point(152, 319)
+        btrDataTypes.Name = "btrDataTypes"
+        btrDataTypes.Size = New Size(254, 152)
+        btrDataTypes.TabIndex = 2
+        btrDataTypes.Text = "Data Type"
+        btrDataTypes.UseVisualStyleBackColor = True
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.Location = New Point(485, 548)
+        btnSubmit.Margin = New Padding(4, 5, 4, 5)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(254, 152)
+        btnSubmit.TabIndex = 3
+        btnSubmit.Text = "Submit"
+        btnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' firstNameTextBox
+        ' 
+        firstNameTextBox.Location = New Point(589, 412)
+        firstNameTextBox.Name = "firstNameTextBox"
+        firstNameTextBox.Size = New Size(150, 31)
+        firstNameTextBox.TabIndex = 4
+        ' 
+        ' firstNameLabel
+        ' 
+        firstNameLabel.AutoSize = True
+        firstNameLabel.Location = New Point(486, 415)
+        firstNameLabel.Name = "firstNameLabel"
+        firstNameLabel.Size = New Size(97, 25)
+        firstNameLabel.TabIndex = 5
+        firstNameLabel.Text = "First Name"
+        ' 
+        ' lastNameLabel
+        ' 
+        lastNameLabel.AutoSize = True
+        lastNameLabel.Location = New Point(488, 462)
+        lastNameLabel.Name = "lastNameLabel"
+        lastNameLabel.Size = New Size(95, 25)
+        lastNameLabel.TabIndex = 6
+        lastNameLabel.Text = "Last Name"
+        ' 
+        ' lastNameTextBox
+        ' 
+        lastNameTextBox.Location = New Point(589, 462)
+        lastNameTextBox.Name = "lastNameTextBox"
+        lastNameTextBox.Size = New Size(150, 31)
+        lastNameTextBox.TabIndex = 7
+        ' 
+        ' ageTextBox
+        ' 
+        ageTextBox.Location = New Point(589, 509)
+        ageTextBox.Name = "ageTextBox"
+        ageTextBox.Size = New Size(150, 31)
+        ageTextBox.TabIndex = 9
+        ' 
+        ' ageLabel
+        ' 
+        ageLabel.AutoSize = True
+        ageLabel.Location = New Point(488, 509)
+        ageLabel.Name = "ageLabel"
+        ageLabel.Size = New Size(44, 25)
+        ageLabel.TabIndex = 8
+        ageLabel.Text = "Age"
+        ' 
+        ' occupationListBox
+        ' 
+        occupationListBox.FormattingEnabled = True
+        occupationListBox.ItemHeight = 25
+        occupationListBox.Items.AddRange(New Object() {"Doctor", "Student", "Nurse", "Teacher", "Cleaner", "Programmer"})
+        occupationListBox.Location = New Point(888, 107)
+        occupationListBox.Name = "occupationListBox"
+        occupationListBox.Size = New Size(180, 179)
+        occupationListBox.TabIndex = 10
+        ' 
+        ' numberTextBox2
+        ' 
+        numberTextBox2.Location = New Point(918, 440)
+        numberTextBox2.Name = "numberTextBox2"
+        numberTextBox2.Size = New Size(150, 31)
+        numberTextBox2.TabIndex = 12
+        ' 
+        ' numberTextBox1
+        ' 
+        numberTextBox1.Location = New Point(918, 390)
+        numberTextBox1.Name = "numberTextBox1"
+        numberTextBox1.Size = New Size(150, 31)
+        numberTextBox1.TabIndex = 11
+        ' 
+        ' twoNumberButton
+        ' 
+        twoNumberButton.Location = New Point(908, 482)
+        twoNumberButton.Margin = New Padding(4, 5, 4, 5)
+        twoNumberButton.Name = "twoNumberButton"
+        twoNumberButton.Size = New Size(176, 85)
+        twoNumberButton.TabIndex = 13
+        twoNumberButton.Text = "Submit"
+        twoNumberButton.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1143, 750)
+        Controls.Add(twoNumberButton)
+        Controls.Add(numberTextBox2)
+        Controls.Add(numberTextBox1)
+        Controls.Add(occupationListBox)
+        Controls.Add(ageTextBox)
+        Controls.Add(ageLabel)
+        Controls.Add(lastNameTextBox)
+        Controls.Add(lastNameLabel)
+        Controls.Add(firstNameLabel)
+        Controls.Add(firstNameTextBox)
+        Controls.Add(btnSubmit)
+        Controls.Add(btrDataTypes)
+        Controls.Add(btnVariables)
+        Controls.Add(btnGo)
+        Margin = New Padding(4, 5, 4, 5)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnGo As Button
+    Friend WithEvents btnVariables As Button
+    Friend WithEvents btrDataTypes As Button
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents firstNameTextBox As TextBox
+    Friend WithEvents firstNameLabel As Label
+    Friend WithEvents lastNameLabel As Label
+    Friend WithEvents lastNameTextBox As TextBox
+    Friend WithEvents ageTextBox As TextBox
+    Friend WithEvents ageLabel As Label
+    Friend WithEvents occupationListBox As ListBox
+    Friend WithEvents numberTextBox2 As TextBox
+    Friend WithEvents numberTextBox1 As TextBox
+    Friend WithEvents twoNumberButton As Button
+
+End Class
---
+++ FirstVBApp/Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim strFirstName = InputBox("Please Enter your first name")
+
+        MessageBox.Show("Welcome to vb.net " & strFirstName)
+
+    End Sub
+
+    Private Sub btnVariables_Click(sender As Object, e As EventArgs) Handles btnVariables.Click
+        Dim strFirstName As String = "John"
+        strFirstName = "Jane"
+
+
+        MsgBox("Hello and welcome " & strFirstName & " I hope you are well")
+    End Sub
+
+    Private Sub btrDataTypes_Click(sender As Object, e As EventArgs) Handles btrDataTypes.Click
+        Dim strMake As String = "Toyota"
+        Dim strModel As String = "Corolla"
+        Dim intDoors As Integer = 4
+        Dim strColour As String = "Blue"
+        Dim boolTaxed As Boolean = True
+        Dim intEngineSize As Integer = 1600
+        Dim decPrice As Decimal = 999.9
+        Dim dateRegistered As Date = #12/25/2020#
+
+
+        MsgBox("The car is a " & strColour & Environment.NewLine & strMake & Environment.NewLine & strModel & " with " & intDoors & " doors. It has an engine size of " & intEngineSize & "cc \n and costs £" & decPrice & ". It was registered on " & dateRegistered & ". Taxed: " & boolTaxed)
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim strFirstName As String
+        Dim strLastName As String
+        Dim intAge As Integer
+        Dim strOccupation As String
+
+        strFirstName = firstNameTextBox.Text
+        strLastName = lastNameTextBox.Text
+        intAge = CInt(ageTextBox.Text)
+        strOccupation = occupationListBox.SelectedItem
+
+
+        MsgBox("Hello " & strFirstName & " " & strLastName & ". You are " & intAge & " years old. " & strOccupation
+               )
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        occupationListBox.Items.Add("Writer")
+        occupationListBox.Items.Add("Actor")
+    End Sub
+
+    Private Sub twoNumberButton_Click(sender As Object, e As EventArgs) Handles twoNumberButton.Click
+        Dim intFirstNumber As Integer
+        Dim intSecondNumber As Integer
+        Dim intTotal As Integer
+
+        If IsNumeric(numberTextBox1.Text) And IsNumeric(numberTextBox2.Text) Then
+            intFirstNumber = CInt(numberTextBox1.Text)
+            intSecondNumber = CInt(numberTextBox2.Text)
+            intTotal = intFirstNumber + intSecondNumber
+            MsgBox("The total is " & intTotal)
+        ElseIf Not IsNumeric(numberTextBox1.Text) And IsNumeric(numberTextBox2.Text) Then
+            MsgBox("Please enter a valid number in the first text box.")
+        Else
+            MsgBox("Please enter valid numbers in both text boxes.")
+        End If
+
+        Select Case intTotal
+            Case Is < 0
+                MsgBox("The total is negative.")
+            Case 5
+                MsgBox("The total is 5.")
+            Case 6 To 10
+                MsgBox("The total is not 0.")
+            Case Else
+                MsgBox("The total is something else.")
+        End Select
+
+        'For i = 1 To 5
+        '    Beep()
+        '    Threading.Thread.Sleep(3000) ' Pause for 3 seconds
+        'Next
+
+        For i = 0 To 5
+            MsgBox("This is loop number " & i)
+        Next
+
+        Dim output As String = ""
+
+        For i = 0 To 10
+            output = output & i & Environment.NewLine
+        Next
+        MsgBox(output)
+
+    End Sub
+End Class
